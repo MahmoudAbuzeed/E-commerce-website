df7aa8c2258cf488d310243d0f26407161d6660e
Mahmoud Abuzeed <mahmoudabozaid56@gmail.com>
2020-12-16 13:15:58 +0200

    initialize server and database connection

---
+++ server/app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+// Database Connection
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("Mongo Database Connected Successfully"))
+  .catch((err) => console.log("Database Not Connected !!!"));
+
+// Middleware
+app.use(morgan("dev"));
+app.use(cookieParser());
+app.use(cors());
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Run Server
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log("Server is running on ", PORT);
+});
